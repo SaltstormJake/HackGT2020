3a46d1cd12721a20871a2d449bd49cb2cf2d2429
Jake Smith <jakeconlonsmith@gmail.com>
2020-10-17 02:19:29 -0500

    player controls

---
+++ Bob Was A Rectangle/Assets/Scripts/ControlledPlayerScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlledPlayerScript : MovementScript
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+            StartCoroutine(Move(MoveDirection.WEST));
+        if (Input.GetKeyDown(KeyCode.D))
+            StartCoroutine(Move(MoveDirection.EAST));
+        if (Input.GetKeyDown(KeyCode.W))
+            StartCoroutine(Move(MoveDirection.NORTH));
+        if (Input.GetKeyDown(KeyCode.S))
+            StartCoroutine(Move(MoveDirection.SOUTH));
+    }
+}
---
+++ Bob Was A Rectangle/Assets/Scripts/MovementScript.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class MovementScript : MonoBehaviour
+{
+    [SerializeField] float movementAmount = 1;
+    [SerializeField] float jumpHeight = 0.5f;
+    [SerializeField] float moveSpeed = 1;
+    private bool isMoving = false;
+    protected enum MoveDirection
+    {
+        NORTH,
+        SOUTH,
+        EAST,
+        WEST
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        isMoving = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    protected IEnumerator Move(MoveDirection direction)
+    {
+        if (!isMoving)
+        {
+            isMoving = true;
+            yield return MoveVertical(true);
+            yield return MoveHorizonal(direction);
+            yield return MoveVertical(false);
+            isMoving = false;
+        }
+    }
+
+    protected IEnumerator MoveVertical(bool direction)
+    {
+        Vector3 position = transform.position;
+        if (direction)
+        {
+            position.y += jumpHeight;
+            while (position.y - transform.position.y > 0.01f)
+            {
+                transform.Translate(Vector3.up * Time.deltaTime, Space.World);
+                yield return null;
+            }
+        }
+        else
+        {
+            position.y -= jumpHeight;
+            while(transform.position.y - position.y > 0.01f)
+            {
+                transform.Translate(Vector3.down * Time.deltaTime, Space.World);
+                yield return null;
+            }
+        }
+        transform.position = position;
+    }
+
+    protected IEnumerator MoveHorizonal(MoveDirection direction)
+    {
+        Vector3 position = transform.position;
+        switch (direction)
+        {
+            case MoveDirection.NORTH:
+                position.z += movementAmount;
+                while (position.z - transform.position.z > 0.01f)
+                {
+                    transform.Translate(Vector3.forward * Time.deltaTime * moveSpeed, Space.World);
+                    yield return null;
+                }
+                break;
+            case MoveDirection.SOUTH:
+                position.z -= movementAmount;
+                while (transform.position.z - position.z > 0.01f)
+                {
+                    transform.Translate(Vector3.back * Time.deltaTime * moveSpeed, Space.World);
+                    yield return null;
+                }
+                break;
+            case MoveDirection.EAST:
+                position.x += movementAmount;
+                while (position.x - transform.position.x > 0.01f)
+                {
+                    transform.Translate(Vector3.right * Time.deltaTime * moveSpeed, Space.World);
+                    yield return null;
+                }
+                break;
+            case MoveDirection.WEST:
+                position.x -= movementAmount;
+                while (transform.position.x - position.x > 0.01f)
+                {
+                    transform.Translate(Vector3.left * Time.deltaTime * moveSpeed, Space.World);
+                    yield return null;
+                }
+                break;
+            default:
+                break;
+        }
+            transform.position = position;
+    }
+}
+
