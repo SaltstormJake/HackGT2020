ab6098af07d3beba95545e1cda7371a496b4ec62
Jimmy <jreilly2112@gmail.com>
2020-10-17 18:58:04 -0400

    Added Main Menu
    Main Menu with three basic functions

---
+++ Bob Was A Rectangle/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField]
+    GameObject panel;
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("SceneTutorial");
+    }
+
+    public void TutorialPanel()
+    {
+        panel.SetActive(!panel.activeSelf);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
