471c6ffd9756e17e375a2cd751e3d72026f7140e
Jimmy <jreilly2112@gmail.com>
2020-10-17 19:26:23 -0400

    Added Pause Menu
    Added a pause menu to the tutorial level

--- Bob Was A Rectangle/Assets/Scripts/ControlledPlayerScript.cs
+++ Bob Was A Rectangle/Assets/Scripts/ControlledPlayerScript.cs
@@ -1,9 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ControlledPlayerScript : MovementScript
 {
+    [SerializeField]
+    GameObject PausePanel;
+
+    [SerializeField]
+    GameObject HowToPlay;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,5 +27,34 @@ public class ControlledPlayerScript : MovementScript
             StartCoroutine(Move(MoveDirection.NORTH));
         if (Input.GetKey(KeyCode.S))
             StartCoroutine(Move(MoveDirection.SOUTH));
+    }
+
+    public void Pause()
+    {
+        PausePanel.SetActive(!PausePanel.activeSelf);
+        HowToPlay.SetActive(false);
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        PausePanel.SetActive(false);
+    }
+
+    public void HowTo()
+    {
+        HowToPlay.SetActive(!HowToPlay.activeSelf);
+        PausePanel.SetActive(false);
+    }
+
+    public void CloseHowTo()
+    {
+        PausePanel.SetActive(!PausePanel.activeSelf);
+        HowToPlay.SetActive(false);
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
     }
 }
