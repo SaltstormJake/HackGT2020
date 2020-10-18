2cf6bef4062a3c7fec63a9c712796e8066f678e1
Jake Smith <jakeconlonsmith@gmail.com>
2020-10-17 21:06:27 -0500

    fixed candle, added darkness script

---
+++ Bob Was A Rectangle/Assets/Scripts/DarknessScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DarknessScript : MonoBehaviour
+{
+    Color lightColor = new Color(RenderSettings.ambientLight.r, RenderSettings.ambientLight.g, RenderSettings.ambientLight.b, RenderSettings.ambientLight.a);
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Darkness()
+    {
+        RenderSettings.ambientLight = Color.black;
+    }
+
+    void Light()
+    {
+        RenderSettings.ambientLight = lightColor;
+    }
+}
