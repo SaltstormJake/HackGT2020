0b8902d859cd5c45de36fa12357ad324f6f9919b
Kevin Qi <kqi30@gatech.edu>
2020-10-18 03:33:25 -0500

    added more light triggers

--- Bob Was A Rectangle/Assets/Scenes/SpeechToText.cs
+++ Bob Was A Rectangle/Assets/Scenes/SpeechToText.cs
@@ -91,7 +91,7 @@ namespace WatsonIntegration
             {"DOWN" , new string[] {"DOWN", "DOWNWARD", "DOWNWARDS", "SOUTH", "SOUTHWARD", "SOUTHWARDS", "SOUTHERLY", "BACKWARDS", "STERN", "STERNWARD", "STERNWARDS"}},
             {"PULL" , new string[] {"PULL", "PUSH", "GRAB", "PAUL", "POLL", "YANK", "TUG", "HEAVE", "LUG"}},
             {"OPEN" , new string[] {"OPEN", "UNLOCK"}},
-            {"LIGHT" , new string[] {"LIGHT", "LIGHTS", "WHITE"}}
+            {"LIGHT" , new string[] {"LIGHT", "LIGHTS", "WHITE", "BRIGHT", "CANDLES"}}
         };
 
         private SpeechToTextService _service;
