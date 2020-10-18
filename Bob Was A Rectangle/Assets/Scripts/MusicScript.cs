16291d443380744ef647051684729eaedc9eda89
Jake Smith <jakeconlonsmith@gmail.com>
2020-10-18 01:12:49 -0500

    soundtrack

---
+++ Bob Was A Rectangle/Assets/Scripts/MusicScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicScript : MonoBehaviour
+{
+    [SerializeField] AudioClip music;
+    private AudioSource sound;
+
+    private void Awake()
+    {
+        sound = gameObject.GetComponent<AudioSource>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        sound.clip = music;
+        sound.loop = true;
+        sound.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
