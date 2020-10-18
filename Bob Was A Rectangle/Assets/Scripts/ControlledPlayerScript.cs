cf16e574705f74149e333b2a1265cec34997a6cc
Jimmy <jreilly2112@gmail.com>
2020-10-18 02:47:33 -0400

    Change Level1

--- Bob Was A Rectangle/Assets/Scripts/ControlledPlayerScript.cs
+++ Bob Was A Rectangle/Assets/Scripts/ControlledPlayerScript.cs
@@ -13,7 +13,8 @@ public class ControlledPlayerScript : MovementScript
     // Start is called before the first frame update
     void Start()
     {
-        
+        PausePanel.SetActive(false);
+        HowToPlay.SetActive(false);
     }
 
     // Update is called once per frame
