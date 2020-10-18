710b93530a46867737244d4663cb9de6cf8dc8e8
Jake Smith <jakeconlonsmith@gmail.com>
2020-10-17 20:30:12 -0500

    level finish

--- Bob Was A Rectangle/Assets/Scripts/LevelCompleteScreenScript.cs
+++ Bob Was A Rectangle/Assets/Scripts/LevelCompleteScreenScript.cs
@@ -5,14 +5,19 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 public class LevelCompleteScreenScript : MonoBehaviour
 {
-    [SerializeField] private Button nextLevelButton;
-    [SerializeField] private Button mainMenuButton;
-    [SerializeField] string nextLevel;
-    [SerializeField] string mainMenu;
+    [SerializeField] private Button nextLevelButton = null;
+    [SerializeField] private Button mainMenuButton = null;
+    [SerializeField] string nextLevel = null;
+    [SerializeField] string mainMenu = null;
     private void Awake()
     {
         nextLevelButton.onClick.AddListener(OnClickNextLevel);
         mainMenuButton.onClick.AddListener(OnClickMainMenu);
+    }
+
+    private void Start()
+    {
+        SetAll(false);
     }
 
     void OnClickNextLevel()
@@ -23,6 +28,13 @@ public class LevelCompleteScreenScript : MonoBehaviour
     void OnClickMainMenu()
     {
         SceneManager.LoadScene(mainMenu);
+    }
+
+    public void SetAll(bool active)
+    {
+        GameObject[] menuObjects = GameObject.FindGameObjectsWithTag("LevelFinishScreenOnly");
+        foreach (GameObject g in menuObjects)
+            g.SetActive(active);
     }
 
 }
